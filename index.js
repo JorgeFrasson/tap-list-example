fcd497489cb99846720b8e20f9a213305549de41
Jorge Henrique Frasson <jorge.frasson@meuchope.com>
2022-10-17 17:58:42 -0300

    Alterações

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3030;
+const port = 3000;
 
 app.get('/', function(req, res) {
     res.sendFile('index.html', {root: __dirname});
