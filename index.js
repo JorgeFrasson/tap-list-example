7f3be72c4f597041185fda8ce1cd182d288042f2
Jorge Henrique Frasson <jorge.frasson@meuchope.com>
2022-10-17 19:22:06 -0300

    Alterações na infra e front

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3030;
 
 app.get("/", function(req, res) {
     res.sendFile("index.html", {root: __dirname});
@@ -9,5 +9,5 @@ app.get("/", function(req, res) {
 app.use(express.static(__dirname + "/static"));
 
 // start the server listening for requests
-app.listen(process.env.PORT || 3000, 
+app.listen(process.env.PORT || port, 
 	() => console.log("Server is running..."));
