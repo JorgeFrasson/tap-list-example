821a4358d7ca8757248d9b022557467e7ca58eab
Jorge Henrique Frasson <jorge.frasson@meuchope.com>
2022-10-24 16:04:29 -0300

    Ajustes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3030;
 
-app.get("/", function(req, res) {
+app.get("/taplist/1", function(req, res) {
     res.send(JSON.stringify({conectou: "filho da puta"}));
 });
 
