c3a7372e725387e6cfc8f9409c18123d43f2e663
Jorge Henrique Frasson <jorge.frasson@meuchope.com>
2022-10-24 16:34:34 -0300

    Ajustes

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 3030;
 
 app.get("/connect/", function(req, res) {
-    res.send(JSON.stringify({conectou: "filho da puta"}));
+    res.send(JSON.stringify({action: "connect"}));
 });
 
 app.get("/disconnect/", function(req, res) {
-    res.send(JSON.stringify({desconectou: "filho da puta"}));
+    res.send(JSON.stringify({action: "disconnect"}));
 });
 
 app.use(express.static(__dirname + "/static"));
