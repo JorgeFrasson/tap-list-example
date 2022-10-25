0c2563dbebcd1235d58602bbdd972e3a5499f079
Jorge Henrique Frasson <jorge.frasson@meuchope.com>
2022-10-25 13:29:45 -0300

    Ajustes

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const port = 3030;
 
 app.use(bodyParser.json());
 
-app.get("/connect", async (req, res) => {
+app.get("/connect", function(req, res){
     console.log("/connect endpoint is invoked");
     res.sendStatus(200);
 });
 
-app.get("/disconnect", async (req, res) => {
+app.get("/disconnect", function (req, res){
     console.log("/disconnect endpoint is invoked");
     res.sendStatus(200);
 });
 
-app.get("/sendtap", async (req, res) => {
+app.get("/sendtap", function(req, res){
     res.send(JSON.stringify({message: "hello from sendtap route"}));
 });
 
-app.post("/send", async (req, res)=> {
+app.post("/send", function(req, res){
     console.log(req.body);
     console.log(req.query);
     console.log(req.headers);
