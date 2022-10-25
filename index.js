5c0ba0f6828f69545f392b4e3decf484f7200448
Jorge Frasson <jorge.frasson147@gmail.com>
2022-10-24 23:48:13 -0300

    Testes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/disconnect/", function(req, res) {
 });
 
 app.get("/", function(req, res){
-    res.send(JSON.stringify(req.body));
+    res.send(JSON.stringify(req.body.action));
 });
 
 
