f88f6cb8e0d1f972e7736d89970b5d616c0c861e
Jorge Henrique Frasson <jorge.frasson@meuchope.com>
2022-10-25 13:24:08 -0300

    AJustes

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3030;
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
 
+app.use(bodyParser.json());
 
-app.get("/connect", function(req, res) {
-    res.send(JSON.stringify({action: "connect filho da puta"}));
+app.get("/connect", async (req, res) => {
+    console.log("/connect endpoint is invoked");
+    res.sendStatus(200);
 });
 
-app.get("/disconnect", function(req, res) {
-    res.send(JSON.stringify("disconnect"));
+app.get("/disconnect", async (req, res) => {
+    console.log("/disconnect endpoint is invoked");
+    res.sendStatus(200);
 });
 
-app.get("/sendtap", function(req, res){
+app.get("/sendtap", async (req, res){
     res.send(JSON.stringify({message: "hello from sendtap route"}));
 });
 
@@ -30,22 +29,6 @@ app.get("/", function(req, res){
     console.log("Tô aqui buceta de dragão");
     res.send(JSON.stringify(req.body));
 });
-
-io.on('connection', (socket) => {
-    socket.on('connect', (msg)=> {
-        console.log(msg);
-    });
-});
-
-
-io.on('connection', (socket) => {
-    console.log("User connected", socket);
-    socket.on('sendmessage', (msg) => {
-        console.log(msg);
-    });
-})
-
-app.use(express.static(__dirname + "/static"));
 
 // start the server listening for requests
 app.listen(process.env.PORT || port, 
