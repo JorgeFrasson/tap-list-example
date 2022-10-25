99f001e51d9ec01753a2903ac1f19b091745f0b4
Jorge Henrique Frasson <jorge.frasson@meuchope.com>
2022-10-25 18:43:07 -0300

    Ajustes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3030;
 const bodyParser = require('body-parser')
+const axios = require('axios');
 
-let connections = [];
-
+let screenDevice = [];
 let API_URL_WSS = 'https://8ffxu1gb54.execute-api.us-east-1.amazonaws.com/dev/@connections'
 
 app.use(bodyParser.json());
@@ -24,10 +24,29 @@ app.post("/disconnect", function (req, res){
 });
 
 app.post("/sendmessage", function(req, res){
+    const connectionId = req.body.connectionID
+    const HEADER = {
+        headers: {
+            Accept: 'application/json',
+            accessKeyId: "AKIA23GEF46NXMHWM3VW",
+            secretAccessKey: "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",
+        }
+    }
+    axios
+        .post(API_URL_WSS + "/" + connectionId, req.body, HEADER)
+        .then((response) => {
+            if(response.status === 201){
+                console.log(response.data);
+                console.log(response.headers);
+            }
+        })
+        .catch((e) => {
+            console.log(e);
+        })
     for(let i = 0; i < connections.length; i ++){
         console.log("O usuário " + connections[i] + " está conectado!");
     }
-    res.send(req.body.message);
+    res.send("Mensagem recebida de " + req.body.connectionId);
 });
 
 app.post("/send", function(req, res){
