919b229d0d1b873623fef0da3e83aeee8b320fbc
Jorge Henrique Frasson <jorge.frasson@meuchope.com>
2022-10-28 17:34:47 -0300

    Ajustando validateToken

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ app.post("/validate-token", async (req, res) => {
 
     try {
         await apigwManagementApi.postToConnection({ 
-            ConnectionId: connectionId,
+            ConnectionId: deviceId,
             Data: postData
         }).promise()
     } catch (e) {
